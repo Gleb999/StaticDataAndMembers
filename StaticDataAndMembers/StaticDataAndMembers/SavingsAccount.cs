5f5ff20c6ff5eaba2e779afdb9faa69e8c8cd1ad
Gleb999 <lihachev99gleb@mail.ru>
2018-02-06 16:10:52 +0700

    Defining Static Fields

---
+++ StaticDataAndMembers/StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class SavingsAccount
+    {
+        public double currBalance;
+
+        public static double currTnterestRate = 0.04;
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+    }
+}
